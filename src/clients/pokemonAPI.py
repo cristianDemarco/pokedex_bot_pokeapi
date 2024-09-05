6ef2e809543e1f0d5dc3b4cfe1a2e701eacf6fec
cristianDemarco <demarcocristian07@gmail.com>
2024-09-05 18:01:47 +0200

    get method and redis cache functionality externalized through CacheManager class

--- src/clients/pokemonAPI.py
+++ src/clients/pokemonAPI.py
@@ -1,80 +1,36 @@
-import requests
-import sys
-import os
-import logging
-import redis
-import json
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from data_models_mapper import PokemonAPIData
-
-r = redis.Redis(host='redis', port=6379, decode_responses=True)
+from models.pokemonAPIData import PokemonAPIData
+from CacheManager import CacheManager
 
 class PokemonAPI:
-    
-    def __init__(self) -> None:
-        self.URL = "https://pokeapi.co/api"
-        self.API_VERSION = "v2"
+
+    def __init__(self):
+        self.cache_manager = CacheManager(URL='https://pokeapi.co/api', API_VERSION = 'v2')
 
     def get_pokemon_count(self) -> int:
-        api = self.get('pokemon-species/?limit=0')
+        api = self.cache_manager.get('pokemon-species/?limit=0')
 
         return api["count"]
 
-    def get(self, api: str) -> dict:        
-        if api.startswith(self.URL):
-            full_url = api
-        else:
-            full_url = '/'.join([self.URL, self.API_VERSION, api])
-
-        # If exists on redis, use that
-
-        #redis.exceptions.ConnectionError
-
-        is_redis_on = True
-
-        try:
-            cache = r.get(full_url)
-            cache = json.loads(cache)
-            logging.info(f"Found {full_url} on redis")
-            return cache
-        except redis.exceptions.ConnectionError:
-            logging.exception("Redis service is not active")
-            is_redis_on = False
-        except Exception as e:
-            logging.exception(e)            
-        
-        logging.info(f"Sending API request to {full_url}")
-        response = requests.get(full_url)
-        response = response.json()
-        
-        logging.info(f"Storing cache for {full_url}")
-
-        if is_redis_on:
-            r.set(full_url, json.dumps(response))
-
-        return response
-
     def get_pokemon_species(self, pokemon: str) -> dict:
         api = f'pokemon-species/{pokemon}'
-        return self.get(api)
+        return self.cache_manager(api)
     
     def get_pokemon_variety(self, pokemon_variety: str) -> dict:
         api = f'pokemon/{pokemon_variety}'
-        return self.get(api)
+        return self.cache_manager(api)
     
     def get_api_data(self, pokemon : str, variety : int = 0) -> PokemonAPIData:
         
-        species_data = self.get_pokemon_species(pokemon=pokemon)
+        species_data = self.cache_manager_pokemon_species(pokemon=pokemon)
 
         if variety > len(species_data["varieties"]) - 1:
             variety = 0
                     
         pokemon_variety = species_data["varieties"][variety]["pokemon"]["name"]
-        pokemon_data = self.get_pokemon_variety(pokemon_variety=pokemon_variety)
+        pokemon_data = self.cache_manager_pokemon_variety(pokemon_variety=pokemon_variety)
         
-        types = self.get_list_of_elements(pokemon_data, "types", "type")
-        abilities = self.get_list_of_elements(pokemon_data, "abilities", "ability")
+        types = self.cache_manager_list_of_elements(pokemon_data, "types", "type")
+        abilities = self.cache_manager_list_of_elements(pokemon_data, "abilities", "ability")
 
         return PokemonAPIData(pokemon_data, species_data, variety, types, abilities)
     
@@ -84,7 +40,7 @@ class PokemonAPI:
 
         for value in data[elements]:
             element_api = value[element]["url"]
-            element_data = self.get(element_api)
+            element_data = self.cache_manager(element_api)
             for item in element_data["names"]:
                 if item["language"]["name"] == language:
                     elements_list.append(item["name"])
