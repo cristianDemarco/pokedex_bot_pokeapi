3db88b350bec61b03586847d7bf76e2be7743acd
cristianDemarco <demarcocristian07@gmail.com>
2024-09-06 14:28:09 +0200

    Search cards list functionality updated

--- src/clients/pokemon_cards_API.py
+++ src/clients/pokemon_cards_API.py
@@ -7,8 +7,10 @@ class PokemonCardsAPI:
     @cached
     def get_cards(self, name) -> None:
         cards = Card.where(q=f'name:{name}')
-        cards = "\n".join(card.name for card in cards)
-        return cards
+        sorted_cards = sorted(cards, key=lambda card: card.set.releaseDate)
+        cards_list = "\n".join(" ".join([card.name, f'[{card.number}/{card.set.total}]']) for card in sorted_cards)
+
+        return cards_list
     
     def get_cards_number(self):
         return len(self.cards)
